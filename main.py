95cb06a9a7f690948a0560beb8f28879ce29901c
Brendan Lamishaw <brendanlamishaw@dhcp-10-103-231-243.wireless.northwestern.private>
2020-02-27 16:51:43 -0600

    krfs

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from scripts import movieWrapper
 
-def createInterface():
+
+def create_interface():
     user_input = ""
 
     while user_input != 'n':
         user_input = input('Enter movie name:\n')
         movie = movieWrapper.Movie(user_input)
-        if not movie.printMovies():
+        if not movie.print_movies():
             continue
         user_input = input('Which movie is the correct one?\n')
 
-        movie.getMovie(user_input)
+        movie.get_movie(user_input)
 
         user_input = input("Do you want to continue? ('y'/'n')\n")
 
+
 def main():
-    createInterface()
+    create_interface()
+
 
 if __name__ == '__main__':
     main()
--- scripts/movieWrapper.py
+++ scripts/movieWrapper.py
@@ -3,6 +3,8 @@ import apiconfig
 
 # Dictionary to map tmdb genre ids to human-readable genres
 
+WRITE_TO_FILE = "krf/moviefacts.krf"
+
 GENRES = {28: "Action",
           12: "Adventure",
           16: "Animation",
@@ -23,6 +25,21 @@ GENRES = {28: "Action",
           10752: "War",
           37: "Western",
         }
+
+DECADES = {
+    "11": "1910",
+    "12": "1920",
+    "13": "1930",
+    "14": "1940",
+    "15": "1950",
+    "16": "1960",
+    "17": "1970",
+    "18": "1980",
+    "19": "1990",
+    "20": "2000",
+    "21": "2010",
+    "22": "2020",
+}
 
 
 class Movie:
@@ -45,6 +62,7 @@ class Movie:
             'genres': None,
             'director': (None, None),
             'cinematographer': (None, None),
+            'composer': (None, None),
             'actors': None,
         }
 
@@ -53,18 +71,16 @@ class Movie:
         # Movies with titles similar to or matching the user input string
         self.matches = search.movie(query=self.input)['results']
 
-
     def __repr__(self):
         print(self.matches)
 
-    def printMovies(self) -> bool:
+    def print_movies(self) -> bool:
         """ Prints the movies with titles matching the user input """
         print("\n****************************************")
 
         if len(self.matches) == 0:
             print(f'No movies with name "{self.input}"')
             return False
-
 
         for i, movie in enumerate(self.matches):
             try:
@@ -76,49 +92,58 @@ class Movie:
         print("****************************************\n")
         return True
 
-
-    def getMovie(self, number):
-        """ This takes a number input by the user from the command line and then selects that movie as the correct title"""
+    def get_movie(self, number):
+        """ This takes a number input by the user from the command line and then selects that movie as the correct title """
         if number.isalpha():
             raise Exception('Must enter a number')
 
         try:
+            # Find the correct movie in self.matches based on user-input number
             self.movie = self.matches[int(number)-1]
 
-            self.info['title'] = (self.movie['id'], self.movie['original_title'])
-            self.info['release_date'] = self.movie['release_date'][:4]
-
+            # Gets the credits of the movie for use in getDirector(), getComposer(), etc.
             self.credits = tmdb.Movies(self.matches[int(number)-1]["id"]).credits()
-            self.getInfo()
+
+            # Calls self.getInfo() to populate the rest of the fields in self.info (i.e. director, actors, composer, etc.)
+            self.get_info()
+
+
         except IndexError:
             raise Exception('Index out of range')
 
-    def getGenres(self):
+    def get_title(self):
+        """ Adds the movie id and original title to self.info """
+
+        self.info['title'] = (self.movie['id'], self.movie['original_title'])
+
+    def get_release_decade(self):
+        """ Gets the decade the movie was released in and stores in self.info """
+
+        release_decade = DECADES[self.movie['release_date'][0] + self.movie['release_date'][2]]
+        self.info['release_date'] = release_decade
+
+    def get_genres(self):
         """ Gets the genre of movie """
 
         genres = [genre for genre in self.movie['genre_ids']]
         self.info['genres'] = genres
 
-    def getDirector(self):
-        """ Gets the director of the movie """
+    def get_crew_members(self):
+        """ Gets the director, cinematographer, and composer of the movie """
 
         for person in self.credits['crew']:
             if person['job'] == "Director":
                 # Store director id and name in tuple
                 self.info['director'] = (person['id'], person['name'])
-                return
-
-    def getCinematorgrapher(self):
-        """ Gets the cinematographer ('Director of Photography') of the movie """
-
-        for person in self.credits['crew']:
+            if person['job'] == "Original Music Composer":
+                # Store composer id and name
+                self.info['composer'] = (person['id'], person['name'])
             if person['job'] == "Director of Photography":
                 # Store cinematographer id and name
                 self.info['cinematographer'] = (person['id'], person['name'])
-                return
 
 
-    def getActors(self):
+    def get_actors(self):
         """ Gets the first 5 returned actors of the movie
             NOTE: this does not mean the 5 most popular or most "significant" actors in the movie
         """
@@ -132,49 +157,76 @@ class Movie:
                 self.info['actors'] = actors
                 return
 
-    def convertInfoToKBFact(self):
+    def convert_info_to_kb_facts(self) -> list:
         """ Converts self.info into facts that can be parsed by Companions """
 
-        print('\n******** KB FACTS **********')
+        movie_id = self.info["title"][0]
         movie_title = self.info["title"][1]
         kb_facts = []
 
-        fact = f'(title "{movie_title}")'
+        fact = f'(isa {movie_id} Movie-CW)'
         kb_facts.append(fact)
+
+        fact = f'(movieTitleString {movie_id} "{movie_title}")'
+        kb_facts.append(fact)
+
 
         for key in self.info.keys():
 
             if key == 'release_date':
-                fact = f'(releaseDate "{movie_title}" {self.info[key]})'
+                fact = f'(movieReleaseDecade {movie_id} {self.info[key]})'
                 kb_facts.append(fact)
+
             if key == 'genres':
                 for genre_id in self.info[key]:
-                    fact = f'(genre "{movie_title}" {GENRES[genre_id]})'
+                    if genre_id == 16:
+                        fact = f'(movieIsAnimated {movie_id})'
+                    else:
+                        fact = f'(movieGenres {movie_id} {GENRES[genre_id] + "Movie"})'
                     kb_facts.append(fact)
+
             if key == 'director':
-                fact = f'(director "{movie_title}" "{self.info[key][1]}"")'
+                fact = f'(movieDirector {movie_id} "{self.info[key][1]}")'
                 kb_facts.append(fact)
+
             if key == 'cinematographer':
 
                 # Some animated movies don't have cinematographers
                 if self.info[key][0] == None:
                     continue
 
-                fact = f'(cinematographer "{movie_title}" "{self.info[key][1]}"")'
+                fact = f'(movieCinematographer {movie_id} "{self.info[key][1]}")'
                 kb_facts.append(fact)
+
+            if key == 'composer':
+                # Some animated movies don't have cinematographers
+                if self.info[key][0] == None:
+                    continue
+
+                fact = f'(movieComposer {movie_id} "{self.info[key][1]}")'
+                kb_facts.append(fact)
+
             if key == 'actors':
                 for actor in self.info[key]:
-                    fact = f'(actorIn "{movie_title}" "{actor[1]}")'
+                    fact = f'(movieActors {movie_id} "{actor[1]}")'
                     kb_facts.append(fact)
-        for item in kb_facts:
-            print(item)
-        print('*************************\n')
 
-    def getInfo(self):
+        return kb_facts
+
+    def write_to_krf(self, kb_facts):
+        file = open(WRITE_TO_FILE, "w")
+        for fact in kb_facts:
+            file.write(fact + '\n')
+        file.close()
+
+    def get_info(self):
         """ Load all info about the movie in JSON format
         """
-        self.getGenres()
-        self.getDirector()
-        self.getCinematorgrapher()
-        self.getActors()
-        self.convertInfoToKBFact()
+        self.get_title()
+        self.get_release_decade()
+        self.get_genres()
+        self.get_crew_members()
+        self.get_actors()
+        kb_facts = self.convert_info_to_kb_facts()
+        self.write_to_krf(kb_facts)
+
